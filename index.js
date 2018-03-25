d26579dd10d23daf178f69655631dc98e47f64c6
lacti <lactrious@gmail.com>
2018-03-25 11:41:35 +0900

    Optimize data syntax

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+const express = require('express')
+const port = process.env.PORT || 3000
+const app = express()
 
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-
-app.use(express.static(__dirname));
-app.listen(port);
+app.use(express.static(__dirname))
+app.listen(port)
+console.log('Waiting on 3000 port.')
--- sosa/game.js
+++ sosa/game.js
@@ -40,13 +40,20 @@ $body.addEventListener('click', $event => {
     return
   }
   if (scene.next) {
-    changeScene(scene.next)
-  } else if (scene.next_if) {
-    for (let each of scene.next_if) {
-      if (eval(each.condition)) {
-        changeScene(each.name)
-        break
+    if (scene.next.constructor.name === 'String') {
+      changeScene(scene.next)
+    } else if (scene.next.constructor.name === 'Array') {
+      for (let each of scene.next) {
+        console.log('next: ' + JSON.stringify(each))
+        const nextScene = Object.keys(each)[0]
+        const condition = each[nextScene]
+        if (eval(condition) /* eslint-disable-line */) {
+          changeScene(nextScene)
+          break
+        }
       }
+    } else {
+      console.log('invalid')
     }
   } else {
     window.alert('No next scene.')
@@ -73,7 +80,7 @@ for (let $button of $buttons) {
     const $currentButtons = getCurrentButtonElements()
     for (let index = 0; index < $currentButtons.length; index++) {
       if ($event.target === $currentButtons[index]) {
-        act(getCurrentSceneData().button[index].action)
+        act(getCurrentSceneData().button[$event.target.innerHTML], 'scene')
         $event.stopPropagation()
         return
       }
@@ -87,10 +94,10 @@ const hideAllButtons = () => {
   }
 }
 const mapButtons = (buttons, $currentButtons) => {
-  for (let index = 0; index < buttons.length; index++) {
-    const button = buttons[index]
-    const $button = $currentButtons[index]
-    $button.innerHTML = button.name
+  let index = 0
+  for (let name of Object.keys(buttons)) {
+    const $button = $currentButtons[index++]
+    $button.innerHTML = name
     $button.setAttribute('style', '')
   }
 }
@@ -101,7 +108,7 @@ const changeCurrentSceneData = name => {
 }
 const getCurrentSceneData = () => ctx.data.scene[ctx.scene]
 const getCurrentButtonElements = () => {
-  const countOfChoices = getCurrentSceneData().button.length
+  const countOfChoices = Object.keys(getCurrentSceneData().button).length
   switch (countOfChoices) {
     case 1:
       return [$button2]
@@ -109,23 +116,23 @@ const getCurrentButtonElements = () => {
       return [$button1, $button2]
   }
 }
-const isFeeDay = () => ((ctx.day - 1) / 7) % 4 === 0
+const isFeeDay = () => ((ctx.day - 1) / 7) % 4 === 0 // eslint-disable-line
+const nl2br = str => (str || '').replace(/\\n/, '<br>')
 const changeScene = (name, moneyChanged) => {
   console.log(name)
   const scene = changeCurrentSceneData(name)
-  if (scene.image) {
-    $image.setAttribute('style', `background-image: url("${scene.image}")`)
-  }
-  if (scene.message.indexOf('${') < 0) {
-    $message.innerHTML = scene.message
-  } else {
-    $message.innerHTML = eval(scene.message)
-  }
+  $image.setAttribute(
+    'style',
+    `background-image: url("${scene.image || `${name}.png`}")`
+  )
+  $message.innerHTML = nl2br(
+    scene.message.indexOf('${') < 0 ? scene.message : eval(scene.message) // eslint-disable-line
+  )
   if (moneyChanged) {
     showMoneyChanged(moneyChanged)
   }
   if (scene.action) {
-    act(scene.action)
+    act(scene.action, 'javascript')
   }
   hideAllButtons()
   if (scene.button) {
@@ -152,37 +159,47 @@ const showMoneyChanged = amount => {
   }
 }
 
-const act = actions => {
+const act = (actions, hint) => {
+  if (actions === undefined || actions === null) {
+    return
+  }
+  if (actions.constructor.name === 'Object') {
+    return act([actions])
+  }
+  if (actions.constructor.name !== 'Array') {
+    return act({ [hint || 'scene']: actions })
+  }
   for (let action of actions) {
     console.log(action)
-    switch (action.type) {
-      case 'javascript':
-        scriptFunctions[action.function]()
-        break
+    for (let [type, value] of Object.entries(action)) {
+      switch (type) {
+        case 'javascript':
+          scriptFunctions[value]()
+          break
 
-      case 'changeScene':
-        changeScene(action.scene)
-        break
+        case 'scene':
+          changeScene(value)
+          break
 
-      case 'addDay':
-        addDay(action.amount)
-        break
+        case 'day':
+          addDay(value)
+          break
 
-      case 'addMoney':
-        addMoney(action.amount)
-        showMoneyChanged(action.amount)
-        ctx.lastMoneyChanged = action.amount
-        break
+        case 'money':
+          addMoney(value)
+          showMoneyChanged(value)
+          break
 
-      case 'result':
-        processResult(action.name)
-        break
+        case 'result':
+          processResult(value)
+          break
+      }
     }
   }
 }
 
 window
-  .fetch('/sosa/data.json')
+  .fetch('data.json')
   .then(e => e.json())
   .then(data => {
     ctx.data = data
@@ -204,25 +221,18 @@ const formatMoney = money => {
   return money >= 0 ? _format(money, 0) : '-' + _format(-money, 0)
 }
 
-const findAction = (actions, type) => {
-  for (let action of actions) {
-    if (action.type === type) {
-      return action
-    }
-  }
-  return null
-}
-
 const processResult = resultName => {
-  const candidates = ctx.data.result[resultName]
+  const result = ctx.data.result[resultName]
+  act(result.action)
+
   let gauge = Math.floor(Math.random() * 100)
-  for (let candidate of candidates) {
-    gauge -= candidate.weight
+  for (let [nextScene, candidate] of Object.entries(result)) {
+    if (!candidate.prob) continue
+
+    gauge -= candidate.prob
     if (gauge <= 0) {
-      act(candidate.action)
-      const moneyAction = findAction(candidate.action, 'addMoney')
-      const moneyChanged = moneyAction ? moneyAction.amount : null
-      changeScene(candidate.scene, moneyChanged)
+      act(candidate.money, 'money')
+      changeScene(nextScene, candidate.money)
       return
     }
   }
